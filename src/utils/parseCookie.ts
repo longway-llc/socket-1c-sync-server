37cacafcd0f8b0fa9cef6f84f4641a2db56c0895
Vadim Korolev <vkorolev1506@gmail.com>
2021-03-15 17:30:20 +0300

    Create parseCookie.ts

---
+++ src/utils/parseCookie.ts
@@ -0,0 +1,17 @@
+import {IncomingMessage} from "http";
+
+export type Cookies = {
+    [key: string]: string
+}
+
+export function parseCookies(request: IncomingMessage): Cookies {
+    const list: {[key:string]: any} = {}
+    const rc = request.headers.cookie
+
+    rc && rc.split(';').forEach((cookie) => {
+        const parts = cookie.split('=')
+        list[`${parts.shift()?.trim()}`] = decodeURI(parts.join('='))
+    })
+
+    return list
+}
